596680e5aa5c9cb9dbcc7d5d6abab3283c96d67d
yash srivastava <yashlala716@gmail.com>
2025-03-08 13:56:28 +0530

    implement login functionality with form validation; add profile and teacher home components

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['student'] ,
+    });
+  }
 
+  loginUser(): void {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Please enter valid email and password.';
+      return;
+    }
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        console.error('Login error:', error);
+        this.errorMessage = error.error.error || 'Invalid email or password';
+      },
+    });
+  }
 }
---
+++ Frontend/src/app/teacher-home/teacher-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-home',
+  imports: [],
+  templateUrl: './teacher-home.component.html',
+  styleUrl: './teacher-home.component.css'
+})
+export class TeacherHomeComponent {
+
+}
